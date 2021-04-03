2cbd6d3bfbed22e8086796359e1e612b751857b2
moyshik7 <moyshik7@gmail.com>
2021-04-03 06:51:23 +0600

    i messed up

---
+++ anime/commands.js
@@ -0,0 +1,13 @@
+const AllCommands = [{
+    name: "Anime",
+    description: "Search any anime or get a random Anime suggestion",
+    usage: ".anime <Name of Anime>",
+    sfw: true,
+    nsfw: false
+}, {
+    name: "Manga",
+    description: "Search any manga/comic",
+    usage: ".manga <Name of Manga / Comic>",
+    sfw: false,
+    nsfw: true
+}];
--- res/main.js
+++ res/main.js
@@ -1,16 +1,3 @@
-const AllCommands = [{
-    name: "Anime",
-    description: "Search any anime or get a random Anime suggestion",
-    usage: ".anime <Name of Anime>",
-    sfw: true,
-    nsfw: false
-}, {
-    name: "Manga",
-    description: "Search any manga/comic",
-    usage: ".manga <Name of Manga / Comic>",
-    sfw: false,
-    nsfw: true
-}];
 let acc = document.getElementsByClassName("accordion");
 let pp = document.getElementsByClassName("pp");
 let commands = document.getElementsByClassName("commands")[0];
