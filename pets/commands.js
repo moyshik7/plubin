01a75cbfaca2a3263559ecee149daf7786767b23
moyshik7 <moyshik7@gmail.com>
2021-04-03 08:09:32 +0600

    finished pets

--- pets/commands.js
+++ pets/commands.js
@@ -1,13 +1,49 @@
 const AllCommands = [{
-    name: "Anime",
-    description: "Search any anime or get a random Anime suggestion",
-    usage: ".anime <Name of Anime>",
+    name: "Cat",
+    description: "Get a cat image",
+    usage: ".cat",
     sfw: true,
     nsfw: false
-}, {
-    name: "Manga",
-    description: "Search any manga/comic",
-    usage: ".manga <Name of Manga / Comic>",
-    sfw: false,
-    nsfw: true
+},{
+    name: "Dog",
+    description: "Get a dog image",
+    usage: ".dog",
+    sfw: true,
+    nsfw: false
+},{
+    name: "Googe",
+    description: "Get a goose image",
+    usage: ".goose",
+    sfw: true,
+    nsfw: false
+},{
+    name: "Fox",
+    description: "Get a fox image",
+    usage: ".fox",
+    sfw: true,
+    nsfw: false
+},{
+    name: "Kitty",
+    description: "Get a kitty image",
+    usage: ".kitty",
+    sfw: true,
+    nsfw: false
+},{
+    name: "Meow",
+    description: "Get a cat image",
+    usage: ".meow",
+    sfw: true,
+    nsfw: false
+},{
+    name: "Lizard",
+    description: "Get a Lizard Image (Warning)",
+    usage: ".lizard",
+    sfw: true,
+    nsfw: false
+},{
+    name: "Puppy",
+    description: "Get image of a puppy",
+    usage: ".puppy",
+    sfw: true,
+    nsfw: false
 }];
