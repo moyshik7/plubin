a227324f779adfd5de738dea47ebb987ba52048a
moyshik7 <moyshik7@gmail.com>
2021-04-03 07:48:21 +0600

    completed maintenance

--- image/commands.js
+++ image/commands.js
@@ -1,19 +1,7 @@
 const AllCommands = [{
-    name: "",
-    description: "",
-    usage: "",
-    sfw: ,
-    nsfw:
-},{
-    name: "Anime",
-    description: "Search any anime or get a random Anime suggestion",
-    usage: ".anime <Name of Anime>",
+    name: "None",
+    description: "jjj",
+    usage: "eee",
     sfw: true,
     nsfw: false
-}, {
-    name: "Manga",
-    description: "Search any manga/comic",
-    usage: ".manga <Name of Manga / Comic>",
-    sfw: false,
-    nsfw: true
 }];
--- maintenance/commands.js
+++ maintenance/commands.js
@@ -1,13 +1,37 @@
 const AllCommands = [{
-    name: "Anime",
-    description: "Search any anime or get a random Anime suggestion",
-    usage: ".anime <Name of Anime>",
+    name: "Avatar",
+    description: "Shows the avatar of any user",
+    usage: ".avatar [User]",
     sfw: true,
     nsfw: false
-}, {
-    name: "Manga",
-    description: "Search any manga/comic",
-    usage: ".manga <Name of Manga / Comic>",
-    sfw: false,
-    nsfw: true
-}];
+},{
+    name: "Emojis",
+    description: "Shows all emojis in a guild",
+    usage: ".emojis",
+    sfw: true,
+    nsfw: false
+},{
+    name: "Help",
+    description: "The help command",
+    usage: ".help",
+    sfw: true,
+    nsfw: false
+},{
+    name: "Invite",
+    description: "Invite the bot to other servers",
+    usage: ".invite",
+    sfw: true,
+    nsfw: false
+},{
+    name: "Ping",
+    description: "Shows the bot ping",
+    usage: ".ping",
+    sfw: true,
+    nsfw: false
+},{
+    name: "Server",
+    description: "Shows the server information",
+    usage: ".server",
+    sfw: true,
+    nsfw: false
+},];
