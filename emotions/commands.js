86a093b317015fd2d15dab6392dc9acf59c8a26c
moyshik7 <moyshik7@gmail.com>
2021-04-03 07:21:09 +0600

    fixed issue with <> and completed emotions

--- emotions/commands.js
+++ emotions/commands.js
@@ -1,13 +1,61 @@
 const AllCommands = [{
-    name: "Anime",
-    description: "Search any anime or get a random Anime suggestion",
-    usage: ".anime <Name of Anime>",
+    name: "Baka",
+    description: "Baka! Baka! Baka !! (Tsundai syndrome)",
+    usage: ".baka [The Baka(Idiot) user]",
     sfw: true,
     nsfw: false
-}, {
-    name: "Manga",
-    description: "Search any manga/comic",
-    usage: ".manga <Name of Manga / Comic>",
-    sfw: false,
-    nsfw: true
-}];
+},{
+    name: "Cuddle",
+    description: "Let's cuddle OwO",
+    usage: ".cuddle [The person you wanna cuddle]",
+    sfw: true,
+    nsfw: false
+},{
+    name: "Feed",
+    description: "Feed me mummy",
+    usage: ".feed [The person you wanna Feed]",
+    sfw: true,
+    nsfw: false
+},{
+    name: "Hug",
+    description: "Hold me tight",
+    usage: ".hug [The Baby]",
+    sfw: true,
+    nsfw: false
+},{
+    name: "Kiss",
+    description: "Chuuuuuuuuuu.......",
+    usage: ".kiss [Your Partner (You can cheat anyway)]",
+    sfw: true,
+    nsfw: false
+},{
+    name: "Kill",
+    description: "R I P",
+    usage: ".kill [The person you wanna kill]",
+    sfw: true,
+    nsfw: false
+},{
+    name: "Pat",
+    description: "Light tap",
+    usage: ".pat [The soft kid]",
+    sfw: true,
+    nsfw: false
+},{
+    name: "Poke",
+    description: "What my mom tells me not to be doing to any girls at the age of 15",
+    usage: ".poke [A Random person]",
+    sfw: true,
+    nsfw: false
+},{
+    name: "Smug",
+    description: "That evil smile",
+    usage: ".smug",
+    sfw: true,
+    nsfw: false
+},{
+    name: "Tickle",
+    description: "Stopppp pleaseee",
+    usage: ".tickle [The person you wanna kill laughing]",
+    sfw: true,
+    nsfw: false
+},];
