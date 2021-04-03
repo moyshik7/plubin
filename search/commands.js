77af233b3a493342495e1d76c61ed50f17d8b3da
moyshik7 <moyshik7@gmail.com>
2021-04-03 08:04:33 +0600

    finished search

--- search/commands.js
+++ search/commands.js
@@ -1,13 +1,37 @@
 const AllCommands = [{
     name: "Anime",
-    description: "Search any anime or get a random Anime suggestion",
-    usage: ".anime <Name of Anime>",
+    description: "Search any anime or get a random anime suggestion",
+    usage: ".anime [Name of Anime]",
     sfw: true,
     nsfw: false
-}, {
-    name: "Manga",
-    description: "Search any manga/comic",
-    usage: ".manga <Name of Manga / Comic>",
+},{
+    name: "Comic",
+    description: "Search any comic",
+    usage: ".comic [Name of Comic]",
     sfw: false,
     nsfw: true
-}];
+},{
+    name: "Define",
+    description: "Search any term / phrase",
+    usage: ".define [Term / Word / Phrase]",
+    sfw: true,
+    nsfw: false
+},{
+    name: "Lyrics",
+    description: "Search for any song Lyrics",
+    usage: ".lyrics [Song Name]",
+    sfw: true,
+    nsfw: false
+},{
+    name: "Manga",
+    description: "Search any Manga / Manhwa (Comics)",
+    usage: ".manga [Name of Manga]",
+    sfw: true,
+    nsfw: false
+},{
+    name: "",
+    description: "",
+    usage: "",
+    sfw: true,
+    nsfw: false
+},];
